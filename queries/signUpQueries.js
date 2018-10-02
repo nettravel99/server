4322b631af066d6f92a8c788437f6dcec82b6c21
George.G <george@adsc.com>
2018-10-02 16:30:31 -0400

    Started add in signup queries - need to finish he actual call to PostGresSql

---
+++ queries/signUpQueries.js
@@ -0,0 +1,55 @@
+var dbmethods = require("../db/db");
+var pswdMethods = require("../factories/utils/passwordEncrypt");
+// var path = require('path');
+
+db = dbmethods.getDB();
+
+function signupUser(req, res, next) {
+  console.log("verify called", req.body);
+  const { credentials } = req.body; // this works because of body-parser middleware.
+  var email = credentials.email;
+  var reqPswd = credentials.password;
+
+  console.log("email", email);
+  console.log("password", reqPswd);
+  // TODO? - email should be transformed to lower case and kept in lower case.
+  //  db.one("select * from users where email = $1", email)
+  db.one("select * from users where email=$1", email)
+    .then(function(data) {
+      console.log("Password is: ", pswdMethods.getPassword(reqPswd, data.salt));
+      // data.salt and data.password are the values in the datatbase. reqPswd is from the user logging in.
+      result = pswdMethods.validate(
+        data.salt,
+        data.password,
+        reqPswd,
+        data.email
+      );
+      console.log("result", result);
+      //var result = true;
+      if (result) {
+        res.status(200).json({
+          status: "success",
+          data: result,
+          message: "Retrieved User Data"
+        });
+        /* jshint ignore:end */
+      } else {
+        res.status(400).json({
+          errors: {
+            global: "Invalid Credentials - 3"
+          }
+        });
+      }
+    })
+    .catch(function(err) {
+      console.log("Error in SQL call");
+      return next(err);
+
+      // res .status(400) .json({status: "fail", data: err, message: "Error retrieving
+      // data"});
+    });
+}
+
+module.exports = {
+  signupUser: signupUser
+};
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var userQueries = require("../queries/USERQUERIES");
+
+router.post("/signup", userQueries.signupUser);
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get("/", function(req, res, next) {
   // Comment out this line: res.send('respond with a resource'); And insert
   // something like this instead:
   res.json([
     {
       id: 1,
       username: "samsepi0l"
-    }, {
+    },
+    {
       id: 2,
       username: "D0loresH4ze"
     }
   ]);
 });
 
-router.get('/dev', function (req, res) {
-  res.send('Hello, you are now on the Dev route!');
+router.get("/dev", function(req, res) {
+  res.send("Hello, you are now on the Dev route!");
 });
 
 module.exports = router;
